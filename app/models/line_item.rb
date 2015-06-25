70fd3c0813ee072cdc06ac9328ade6fca8eeb069
Rafael Ribeiro <rafael.ribeirorj@gmail.com>
2015-06-25 15:22:06 -0300

    E-commerce livros

---
+++ app/models/line_item.rb
@@ -0,0 +1,10 @@
+class LineItem < ActiveRecord::Base
+
+  belongs_to :order
+  belongs_to :product
+  belongs_to :cart
+  attr_accessible :product_id, :cart_id
+  def total_price
+    product.price * quantity
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '900effb93c24364a9b7d0fce4d36bead4a9ffcdd945495f3c06570e7b71e66c702c0cdf9f548e14b4e522969994543e966e89db14979dab42497cc99f2eb7b80'
